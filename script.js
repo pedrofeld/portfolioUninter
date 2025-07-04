a3de465fe482b36d8c3a1518a4024c7e5ec550c7
Pedro Feld <pedro.gael.feld@gmail.com>
2025-07-03 22:43:13 -0300

    feat: início da seção de projetos (apresentados através de modais)

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+// função para alternar menu na versão mobile
 function toggleMenu() {
     const menu = document.getElementById("menu");
     menu.classList.toggle("show");
     console.log('Menu toggled');
 }
+
+/*
+// função para abrir e fechar modais
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// fecha o modal se o usuário clicar fora dele
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+        if (event.target === modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
+*/
